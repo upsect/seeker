ed4c384e1556e82c530e5541a746612c7727828a
Nathan Allen <nallenscott@me.com>
2020-09-17 10:21:41 -0400

    Initial commit

---
+++ test/seek.spec.js
@@ -0,0 +1,28 @@
+const { expect, fancy } = require('fancy-test')
+const { seek } = require('../lib')
+
+describe('exists', () => {
+  fancy
+    .add('module', seek.exists('mocha'))
+    .it('returns true if the module is resolvable', ctx => {
+      expect(ctx.module).to.be.true
+    })
+  fancy
+    .add('module', seek.exists('foobar'))
+    .it('returns false if module is not resolvable', ctx => {
+      expect(ctx.module).to.be.false
+    })
+})
+
+describe('import', () => {
+  fancy
+    .add('module', seek.import('foobar', 'os'))
+    .it('returns with the first module that resolves', ctx => {
+      expect(ctx.module).to.have.property('platform')
+    })
+  fancy
+    .add('module', seek.import('foobar', 'barfoo'))
+    .it('returns null if all modules fail to resolve', ctx => {
+      expect(ctx.module).to.be.null
+    })
+})
