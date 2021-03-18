56dfc160d0a1d56405440bef09fac683614e1e21
Nathan Allen <nallenscott@me.com>
2021-03-18 17:22:50 -0400

    Fix linting in spec file

--- test/seek.spec.js
+++ test/seek.spec.js
@@ -1,28 +1,28 @@
-const { expect, fancy } = require('fancy-test')
-const { seek } = require('../lib')
+const { expect, fancy } = require( 'fancy-test' );
+const { seek } = require( '../lib' );
 
-describe('exists', () => {
+describe( 'exists', () => {
   fancy
-    .add('module', seek.exists('mocha'))
-    .it('returns true if the module is resolvable', ctx => {
-      expect(ctx.module).to.be.true
-    })
+    .add( 'module', seek.exists( 'mocha' ))
+    .it( 'returns true if the module is resolvable', ctx => {
+      return expect( ctx.module ).to.be.true;
+    });
   fancy
-    .add('module', seek.exists('foobar'))
-    .it('returns false if module is not resolvable', ctx => {
-      expect(ctx.module).to.be.false
-    })
-})
+    .add( 'module', seek.exists( 'foobar' ))
+    .it( 'returns false if module is not resolvable', ctx => {
+      return expect( ctx.module ).to.be.false;
+    });
+});
 
-describe('import', () => {
+describe( 'import', () => {
   fancy
-    .add('module', seek.import('foobar', 'os'))
-    .it('returns with the first module that resolves', ctx => {
-      expect(ctx.module).to.have.property('platform')
-    })
+    .add( 'module', seek.import( 'foobar', 'os' ))
+    .it( 'returns with the first module that resolves', ctx => {
+      return expect( ctx.module ).to.have.property( 'platform' );
+    });
   fancy
-    .add('module', seek.import('foobar', 'barfoo'))
-    .it('returns null if all modules fail to resolve', ctx => {
-      expect(ctx.module).to.be.null
-    })
-})
+    .add( 'module', seek.import( 'foobar', 'barfoo' ))
+    .it( 'returns null if all modules fail to resolve', ctx => {
+      return expect( ctx.module ).to.be.null;
+    });
+});
