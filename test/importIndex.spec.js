7da307ee9f7f7946fcd78f439847399d7529ead9
Nathan Allen <nallenscott@me.com>
2021-03-22 07:00:55 -0400

    Add class handling

--- test/importIndex.spec.js
+++ test/importIndex.spec.js
@@ -13,9 +13,11 @@ describe( 'import index', () => {
     .it( 'returns null for non-cjs modules or undefined exports' );
   fancy
     .add( 'exports', Seeker.import({ index: `${process.cwd()}/test/fixtures/modules` }))
+    .do( ctx => expect( ctx.exports.array ).to.be.a( 'array' ))
+    .do( ctx => expect( ctx.exports.class ).to.be.a( 'function' ))
+    .do( ctx => expect( ctx.exports.function ).to.be.a( 'function' ))
     .do( ctx => expect( ctx.exports.object ).to.be.a( 'object' ))
     .do( ctx => expect( ctx.exports.object.foo ).to.be.a( 'function' ))
     .do( ctx => expect( ctx.exports.object.bar ).to.be.a( 'function' ))
-    .do( ctx => expect( ctx.exports.function ).to.be.a( 'function' ))
     .it( 'returns cjs modules with plain object and function exports' );
 });
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -2,6 +2,10 @@ const { expect, fancy } = require( 'fancy-test' );
 const Utils = require( './../lib/utils' );
 
 
+class Foo { get () {} set () {} }
+class Bar extends Foo { get () {} set () {} }
+
+
 describe( 'utils', () => {
   describe( 'readDir', () => {
     fancy
@@ -12,5 +16,18 @@ describe( 'utils', () => {
       .add( 'dirents', Utils.readDir( `${process.cwd()}/test/fixtures/modules` ))
       .do( ctx => expect( ctx.dirents ).to.be.a( 'array' ))
       .it( 'returns dirents if path contains files' );
+  });
+
+  describe( 'isClass', () => {
+    fancy
+      .do(() => expect( Utils.isClass( Foo )).to.be.true )
+      .do(() => expect( Utils.isClass( Bar )).to.be.true )
+      .it( 'returns true if function is a class' );
+    fancy
+      .do(() => expect( Utils.isClass( function () {})).to.be.false )
+      .do(() => expect( Utils.isClass( function () { 'use strict'; })).to.be.false )
+      .do(() => expect( Utils.isClass(() => {})).to.be.false )
+      .do(() => expect( Utils.isClass({ hello: 'world' })).to.be.false )
+      .it( 'returns false if function is not a class' );
   });
 });
